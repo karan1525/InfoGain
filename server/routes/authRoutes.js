51923959ea9de8c032a2e428343cfd8ba040e681
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-02 22:31:58 -0800

    Added authentication and testing logic

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,8 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/current_user', (req, res) => { //req -> incoming, res -> outgoing
+    res.send(req.user);
+  });
+
 };
