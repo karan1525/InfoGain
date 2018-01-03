1cc10c40668d8d9b36720516f9e6cf6f30c940cf
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-02 21:47:50 -0800

    Added Mangoose query logic to only add user if not already in DB

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,17 @@ passport.use(
     callbackURL: '/auth/google/callback'
   },
     (accessToken, refreshToken, profile, done) => {
-      new User ({ googleId: profile.id }).save();
+
+      User.findOne({ googleId: profile.id })
+        .then( (existingUser) => {
+          if (existingUser) {
+            // we already have the record with the given profile ID
+          } else {
+            // we don't have a user with this ID. Make a new record
+            new User ({ googleId: profile.id }).save();
+          }
+        })
+
     }
   )
 );
