c46495bba93424bd11d518659f05af7d995b7585
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-04 14:44:34 -0800

    Added header css styling

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapped">
+          <a className="left brand-logo">
+            Infogain
+          </a>
+          <ul className="right">
+            <li>
+              <a>
+                Login With Google
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
