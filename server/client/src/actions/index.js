eaf961d2f1c4287602333e8edabac523a997d687
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-04 15:52:24 -0800

    Async/ Await syntax refactoring

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-	// returning fetch_user action
-	// return action until ajax request is completed
-  return function(dispatch) {
-    axios
-    .get('/api/current_user')
-    .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  }
+// returning fetch_user action
+// return action until AJAX request is completed
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res });
 };
