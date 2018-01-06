4668112afca68db190efe2a84c498179dcab9af4
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-05 20:16:27 -0800

    Fixed payments button UI

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -6,10 +6,16 @@ class Payments extends Component {
 
     return (
         <StripeCheckout
+          name="Infogain"
+          description="$5 for 5 email credits"
           amount={500} //in cents ¢
           token={token => console.log(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        />
+        >
+          <button className="btn">
+            Add Credits
+          </button>
+        </StripeCheckout>
     );
   }
 }
