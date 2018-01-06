9e369664a7f06635c4b874bc3dc933d4122c2db1
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-05 19:46:33 -0800

    Added production and dev keys for Stripe in client

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -12,4 +12,8 @@ const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}><App /></Provider>,
-	 document.querySelector('#root'));
+	 document.querySelector('#root')
+ );
+
+ console.log('STRIKE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+ console.log('ENVORIMENT IS', process.env.NODE_ENV);
