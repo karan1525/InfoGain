39e8dbc1a44956857cc8ae2e1e6bef50e418c8f5
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-05 20:46:12 -0800

    Added bodyParser middleware to the backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User.js');
 require('./services/passport.js');
@@ -9,6 +10,8 @@ require('./services/passport.js');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession( {
