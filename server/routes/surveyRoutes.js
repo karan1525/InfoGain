a1f9a2e3261f06c95f6af68c5459e7ea14eb5cec
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-07 14:36:00 -0800

    Removed extra whitespace

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -11,7 +11,6 @@ module.exports = app => {
     res.send('Thanks for voting!');
   });
 
-
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {title, subject, body, recipients } = req.body;
 
