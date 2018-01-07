6deaaba551f02f90f34e7260c21906d7e80ea8e5
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-06 19:37:47 -0800

    Boilerplate for sending emails

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -11,8 +11,26 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); //helper.Mail function -> addContent
+    this.addClickTracking();
+    this.addRecipients();
+
   }
 
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 }
 
 module.exports = Mailer;
