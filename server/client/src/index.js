07bed29bfc2cd875f32ba8468be1ff07c3e31e74
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-06 20:06:09 -0800

    Added code to test sending email to sendGrid

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,8 @@ import { Helmet } from 'react-helmet';
 
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
