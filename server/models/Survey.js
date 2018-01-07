0d8789f5ac7249749068a7bf06c4928493b49e99
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-06 16:14:12 -0800

    Fixed comment

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -6,7 +6,7 @@ const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [RecipientSchema], // an array of Strings
+  recipients: [RecipientSchema], // an array that conforms to RecipientSchema
   yes: { type: Number, default: 0},
   no: { type: Number, default: 0}
 });
