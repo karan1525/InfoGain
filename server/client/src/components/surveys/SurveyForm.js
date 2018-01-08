5c0886d6fa98e0efd3ad54cee168eb598192ff98
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-08 00:00:34 -0800

    Added valid email checking

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,57 +1,83 @@
 // SurveyForm shows a form for a user to add input
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
-import { Link } from 'react-router-dom';
-import SurveyField from './SurveyField';
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
-	{label: 'Survey Title', name: 'title', noValueError: 'Provide a survey title!'},
-	{label: 'Subject Line', name: 'subject', noValueError: 'Provide a subject line!'},
-	{label: 'Email Body', name: 'body', noValueError: 'Provide an email body!'},
-	{label: 'Recipient List', name: 'emails', noValueError: 'Provide some emails!'}
+  {
+    label: "Survey Title",
+    name: "title",
+    noValueError: "Provide a survey title!"
+  },
+  {
+    label: "Subject Line",
+    name: "subject",
+    noValueError: "Provide a subject line!"
+  },
+  {
+    label: "Email Body",
+    name: "body",
+    noValueError: "Provide an email body!"
+  },
+  {
+    label: "Recipient List",
+    name: "emails",
+    noValueError: "Provide some emails!"
+  }
 ];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
 
-	renderFields() {
-		return _.map(FIELDS, ({label, name}) => {
-			return <Field key={name} component={SurveyField} type="text" label={label} name={name}/>
-		});
-	}
-
-	render() {
-			return (
-				<div className="container">
-					<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-						{this.renderFields()}
-						<Link to="/surveys" className="red btn-flat white-text">
-						Cancel
-					</Link>
-					<button type="submit" className="teal btn-flat right white-text">
-						Next
-						<i className="material-icons right">done</i>
-					</button>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 // values -> an obj that contains all the parts of the form
 function validate(values) {
-	const errors = {};
+  const errors = {};
 
-	_.each(FIELDS, ({ name, noValueError}) => {
-		if(!values[name]) {
-			errors[name] = noValueError
-		}
-	});
+  errors.emails = validateEmails(values.emails || "");
 
-	return errors;
+  _.each(FIELDS, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
+
+  return errors;
 }
 
 export default reduxForm({
-	validate,
-	form: 'surveyForm'
+  validate,
+  form: "surveyForm"
 })(SurveyForm);
---
+++ server/client/src/utils/validateEmails.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default recipients => {
+  const invalidEmails = recipients
+    .split(",")
+    .map(email => email.trim())
+    .filter(email => !re.test(email));
+
+  var size = invalidEmails.length;
+
+  if (
+    invalidEmails[size - 1] === "" ||
+    recipients.substr(recipients.length - 1) === ","
+  ) {
+    return `Please do not submit trailing commas in your Recipient List`;
+  }
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+
+  return;
+};
