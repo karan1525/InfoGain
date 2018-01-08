549781b67db71d505da32fbd585fd9dcaf265774
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-07 16:44:36 -0800

    Removed reduxHelmet and added dashboard button

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { Helmet } from 'react-helmet';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -16,9 +15,6 @@ window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-	<div>
-		<Helmet><title>InfoGain</title></Helmet>
-		<Provider store={store}><App /></Provider>
-	</div>,
+	<Provider store={store}><App /></Provider>,
 	 document.querySelector('#root')
  );
