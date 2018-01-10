033ae4ce98bf1b62759bb42b5d00a88903ddceeb
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-10 11:48:06 -0800

    Code cleanup

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -15,11 +15,19 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, event => {
-      const path = new URL(event.url).pathname;
+    const events = _.map(req.body, ({ email, url }) => {
+      const path = new URL(url).pathname;
       const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(path));
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
     });
+    console.log(events);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
